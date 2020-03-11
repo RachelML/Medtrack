bcd4fd815da97e2e9d87af0b22992d0cd9a64229
Rachel Long <rachellong@Rachels-MacBook-Pro.local>
2020-03-11 16:54:03 -0400

    home images

--- medtrack/client/src/components/Home.jsx
+++ medtrack/client/src/components/Home.jsx
@@ -1,12 +1,29 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
+import bottleone from '../images/bottleone.png'
+import bottletwo from '../images/bottletwo.png'
+import bottlethree from '../images/bottlethree.png'
+
+
 
 function Home(props) {
   return (
-      <div>
-          <p>Easily track your medications</p>
+      <div className="home-container">
+          <p className="slogan">Easily track your medications</p>
+          
+          <div className="bottle-container">
+          <img className="bottleone" src={bottleone} />
+          <img className="bottletwo" src={bottletwo} />
+          <img className="bottlethree" src={bottlethree} />
+          <img className="bottlefour" src={bottleone} />
+          </div>
+
+
+
           <button onClick={props.handleRegisterButton}  className="sign-in-now">Sign up now</button>
           <button  onClick={props.handleLoginButton} className="log-in-now">Login</button>
+
+
       </div>
     
   )
